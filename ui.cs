594a56dd2a2f869a60d61cbbc36c32ac6cb8aa7a
tattianacruz <lcruzdiaz7164@conestogac.on.ca>
2025-02-18 22:19:13 -0500

    Implementing UI and input management class
    this class contains statistical techniques for console message display, begin by using only the display() method, which enables us to show the most important messages for user interaction

---
+++ ui.cs
@@ -0,0 +1,21 @@
+﻿/* 
+* file : UI.cs
+* project : prog1385 - object oriented programming
+* programmer : luisa tatiana cruz diaz
+* first version : 2024-02-18
+* description :
+* this program defines a static class 'UI' that provides a simple method to display messages
+* to the console. it is intended to handle basic output operations for the project.
+*/
+
+using System;
+
+public static class UserInterface
+{
+    // displays a message to the console
+
+    public static void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
