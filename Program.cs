18900280486a4ce6113e4dcd0d767f7526093af0
tattianacruz <lcruzdiaz7164@conestogac.on.ca>
2025-02-18 22:42:54 -0500

    execute the program
    This merely handles the run class's instantiation and the Execute() method's execution, the program's primary entry point is this file

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+* file : program.cs
+* project : prog1385 - object oriented programming
+* programmer : luisa tatiana cruz diaz
+* first version : 2024-02-18
+* description :
+* this is the entry point of the application. it creates an instance of the 'run' class
+* and starts the execution of the program by calling the 'execute' method.
+*/
+
+using System;
+
+public class Program
+{
+    // main method: entry point of the program
+    public static void Main()
+    {
+        // create an instance of the 'run' class to manage members
+        Run memberManager = new Run();
+
+        // start the execution of the menu-driven program
+        memberManager.Execute();
+    }
+}
